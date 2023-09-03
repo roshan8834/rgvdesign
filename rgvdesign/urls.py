614ca7d8f5ef92655c07b62f5af6686df61ee718
roshan8834 <roshansetfly@gmail.com>
2023-09-03 17:55:26 +0530

    commit

--- rgvdesign/urls.py
+++ rgvdesign/urls.py
@@ -19,6 +19,8 @@ from django.urls import path
 from rgvdesign import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import include, re_path
 
 #from django.views.static import serve
 #from django.conf.urls import url
@@ -33,9 +35,11 @@ urlpatterns = [
     path('contact/',views.contactUs),
     path('service/',views.services),
     path('saveenquiry/',views.saveEnquiry,name='saveenquiry'),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     #url(r'^media/(?p<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
     #url(r'^static/(?p<path>.*)$',serve,{'document_root': settings.STATIC_ROOT}),
 ]
 #urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
